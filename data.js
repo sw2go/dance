f74159d53219a82aceb90a013318e2157f6959a5
aa <aac1291@gmx.ch>
2025-10-26 18:40:45 +0100

    update

--- data.js
+++ data.js
@@ -312,10 +312,15 @@ tips: [
 ]
 },
 {
-head: { title: "HOD Tango", sub: "" },
+head: { title: "HOD Tango", sub: "Outside Swivel" },
 feet:  "LF-F.svg",
 tips: [
-  { title: "Zählung", text: "?", url: "ZG3shcJn-qxRVfpvz.html" }
+  { title: "Zählung",    text: "?", url: "ZG3shcJn-qxRVfpvz.html" },
+  { title: "Einleitung", text: "Öffnen wie für Promenade, Slow (wait), (ich glaube Slow, Quck Quick Slow) 4 Schritte links aussen vorbei, Dame am Ende rückwärts" },
+  { title: "Swivel",     text: "Beginn Herr steht auf vorne rechts, Dame auf hinten links, dann 3 Swivel Quick - Quick" },
+  { title: "Abschluss",  text: "Beim 3.ten Swivel (Herr wippt zurück und belastet linken Fuss hinten), innen beginnend Schritt, Tap, Slow, Ab-Schluss Schritt"  },
+  
+  { title: "Rythmus", url: "https://youtu.be/GH6uMJdGkvE" }
 ]
 },
 {
@@ -326,25 +331,27 @@ tips: [
 ]
 },
 {
-head: { title: "HOD Bachata", sub: "Promenade, Körbli-Stuff" },
+head: { title: "HOD Bachata", sub: "Outside, Inside Turn, Promenade, Körbli-Stuff" },
 feet:  "LF-L.svg",
 tips: [
   { title: "Grundschritt",  text: "1 2 3 tap, 5 6 7 tap", url: "xJlGgJ16-qxRVfpvz.html" },
   { title: "Vor-Zurück",    text: "Einfach mit dem Tap Fuss vor, beachte: Den Tap macht man immer da wo der Fuss gerade ist (kein Schritt)" },
-  { title: "Drehungen",     text: "Dame wie Herr können unter dem Arm des Herrn durch und wieder zurück" },
+  { title: "Drehungen",     text: "Outside und Inside Turn, Dame wie Herr können unter dem Arm des Herrn hin (Outside) und zurück (Inside) drehen" },
+  { title: "Outside",       text: "Nach links gehen vorwärts auf 1 90° auf 2 180° und rückwärts auf 3 270° drehen dann bereits rechts den Tap in die Gegenrichtung" },
+  { title: "Inside",        text: "Nach dem Outside kann man direkt zurück mit dem Inside turn oder auch normal Grundschritt weiter" },
   { title: "Promenade",     text: "" },
   { title: " Vorbereitung",  text: "Die Hand der Dame hinter ihrem Rücken an die rechte Hand des Herrn übergeben" },
   { title: "Drehung+Flirt", text: "Dame dreht und ihre Hand hinter den Kopf des Herrn bringen und loslassen " },
-  { title: "Promenade",     text: "Gemeinsam gehen, 1 2 3 (auf 3 drehen und Tap in die andere Richtung)" },
+  { title: "Promenade",     text: "Gemeinsam nach rechts gehen, 1 2 3 (auf 3 drehen und Tap in die andere Richtung)" },
   { title: "Richtungsw.",   text: "Wenn Herr links vor geht beim 3. Schritt links an Dame 90 vorbei drehen" },
   { title: "Richtungsw.",   text: "Wenn Herr rechts zurück geht beim 3. Schritt die Dame 90 rechts vorbei drehen" },
   { title: "Körbli-Stuff",  text: "" },
   { title: "Vorbereitung",  text: "Inside Turn und dann 2 Hand Haltung" },
   { title: "Körbli",        text: "Dame ins Körbli" },
   { title: "Vor-Zurück",    text: "Zusammen im Körbli vor und zurück" },
-  { title: "Hin und her",   text: "Dame von rechts im Körbli nach links in Körbli und wieder zurück" },
-  { title: "Ausdrehen",     text: "Mit der linken an Schulter von Dame und auswickeln" },
-  { title: "Frisbee",       text: "Mit der rechten Hand Dame nach aussen drehen und loslassen" }
+  { title: "Hin und her",   text: "Dame von rechts im Körbli nach links ins Körbli und wieder zurück" },
+  { title: "Ausdrehen",     text: "Mit der linken Hand an die Schulter der Dame und Impuls zum auswickeln" },
+  { title: "Frisbee",       text: "dann mit der rechten Hand die Dame nach aussen drehen und loslassen" }
   
 ]
 },
